a65290752deb270cd421bc505b37defadd66afa7
shadowmoses26 <dracula094@mail.ru>
2024-03-15 15:00:55 +0300

    Keep values on post

--- app.rb
+++ app.rb
@@ -37,6 +37,7 @@ get '/' do
 end
 
 get '/visit' do
+	@c = Client.new
 	erb :visit
 end
 
@@ -46,12 +47,12 @@ end
 
 post '/visit' do
 
-	c = Client.new params[:client]
+	@c = Client.new params[:client]
 
-	if c.save
+	if @c.save
 		erb "<h2> Cпасибо, вы записаны!</h2>"
 	else
-		@error = c.errors.full_messages.first
+		@error = @c.errors.full_messages.first
 		erb :visit
 		end
 
